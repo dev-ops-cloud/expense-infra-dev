de4907b96375b087d46d35ff27061dd8b567db26
Pallavi C <info@joindevops.com>
2025-02-16 19:05:07 +0530

    terraform ansible

---
+++ 60-backend/backend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dnf install ansible -y
+
+# push
+# ansible-playbook -i inventory mysql.yaml
+
+# pull
+ansible-pull -i localhost, -U https://github.com/dev-ops-cloud/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
---
+++ 60-backend/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+   default = "expense"
+}
+
+variable "environment" {
+   default = "dev"
+}
+
+
+
+variable "common_tags" {
+    default = {
+        project = "expense"
+        environment = "dev"
+        terraform = "true"
+    }
+}
+
+# variable "zone_id" {
+#    default = "Z05090632KRW9DIHOUMAP"
+# }
+
+# variable "domain_name" {
+#    default = "rushika.site"
+# }
+
+# variable "zone_name" {
+#    default = "rushika.site"
+# }
